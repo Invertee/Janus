960bb75b1e5bb5106696b9208b3f5f334b59ceae
Sam Petch <smpetch+github@gmail.com>
2019-07-31 07:48:20 +0100

    Added keyboard shortcuts and a hide button

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,5 @@
+var pageState = ''
+var hidden = false
 function fetchWrapper(url, options, timeout) 
 {
     return new Promise((resolve, reject) => {
@@ -10,72 +12,94 @@ function fetchWrapper(url, options, timeout)
     });
 }
 
-document.getElementById('encrypt').addEventListener('click', function() 
-{
-    fetchWrapper( '/encrypt', 
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                input: (document.getElementById('input').value),
-            })
-            }, 10000 )
-            .then(data => {
-                return data.json();
-            })
-            .then(data => {
-                if (data.status === 'okay')
-                {
-                    document.getElementById('input').value = data.result
-                } else {
-                    let div = document.getElementById('input')
-                    div.value = data.result
-                        div.classList.add('flash');
-                        setTimeout(function() {
-                            div.classList.remove('flash');
-                        }, 500);
-                }
-            })
-            .catch(err => console.error(err))
-});
 
-document.getElementById('decrypt').addEventListener('click', function() 
-{
-    fetchWrapper( '/decrypt', 
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                input: (document.getElementById('input').value),
-            })
-            }, 10000 )
-            .then(data => {
-                return data.json();
-            })
-            .then(data => {
-                if (data.status === 'okay')
-                {
-                    document.getElementById('input').value = data.result
-                } else {
-                    let div = document.getElementById('input')
-                    div.value = data.result
-                        div.classList.add('flash');
-                        setTimeout(function() {
-                            div.classList.remove('flash');
-                        }, 500);
-                }
-            })
-            .catch(err => console.error(err))
-});
 
-document.getElementById('copybutton').addEventListener('click', function(){
-    
+function submit(action) {
+    fetchWrapper( `/${action}`, 
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            input: (document.getElementById('input').value),
+        })
+        }, 10000 )
+        .then(data => {
+            return data.json();
+        })
+        .then(data => {
+            pageState = action
+            if (data.status === 'okay')
+            {   
+                let inp = document.getElementById('input')
+                inp.value = data.result
+                copyText('input')
+            } else {
+                document.getElementById('input').value = data.result
+                addFlash('input')
+            }
+        })
+        .catch(err => console.error(err))
+}
+
+function copyText(elementID) {
+    var copyText = document.getElementById(elementID);
+    if (hidden) {
+        toggleHidden()
+        copyText.select();
+        document.execCommand("copy");
+        toggleHidden()
+    } else {
+        copyText.select();
+        document.execCommand("copy");
+    }
+}
+
+function toggleHidden() {
+    if (hidden) {
+        hidden = false
+        document.getElementById('input').classList.remove('hidden')
+        document.getElementById('hidebutton').innerText = '🙉'
+    } else {
+        hidden = true
+        document.getElementById('input').classList.add('hidden')
+        document.getElementById('hidebutton').innerText = '🙈'
+    }
+}
+
+function addFlash(elementID) {
+    let div = document.getElementById(elementID)
+    div.classList.add('flash');
+    setTimeout( () => {
+        div.classList.remove('flash');
+    }, 500);
+}
+
+document.getElementById('encrypt').addEventListener('click', () => submit("encrypt") );
+
+document.getElementById('decrypt').addEventListener('click', () => submit("decrypt") );
+
+document.getElementById('copybutton').addEventListener('click', () => {
     document.getElementById('input').select();
-
-    document.execCommand("copy")
-  
+    copyText('input')
 });
+
+document.getElementById('hidebutton').addEventListener('click', () => toggleHidden() )
+
+const buttons = document.querySelectorAll(".button")
+for (const button of buttons) {
+  button.addEventListener('click', () => {
+    addFlash(button.id)
+  })
+}
+
+document.onkeyup = (e) => {
+    if (e.ctrlKey && e.which == 66) {
+        submit('encrypt');
+        addFlash('encrypt')
+    } else if (e.ctrlKey && e.which == 77) {
+        submit('decrypt');
+        addFlash('decrypt')
+    }
+  };
